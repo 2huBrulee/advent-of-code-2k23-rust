0896c1185b75b22d755b9f552311a0f4e4fe28ae
Alejandro Merino <alejandro2hpxa@gmail.com>
2023-12-05 14:17:16 -0500

    day 1 part 1

---
+++ day1/trebuchet/target/debug/deps/trebuchet-4a70f53d027195fb.d
@@ -0,0 +1,5 @@
+/Users/ale/Workspace/projects/advento2k23-rust/day1/trebuchet/target/debug/deps/trebuchet-4a70f53d027195fb: src/main.rs
+
+/Users/ale/Workspace/projects/advento2k23-rust/day1/trebuchet/target/debug/deps/trebuchet-4a70f53d027195fb.d: src/main.rs
+
+src/main.rs:
---
+++ day1/trebuchet/target/debug/trebuchet.d
@@ -0,0 +1,1 @@
+/Users/ale/Workspace/projects/advento2k23-rust/day1/trebuchet/target/debug/trebuchet: /Users/ale/Workspace/projects/advento2k23-rust/day1/trebuchet/src/main.rs
